5fb0b6e51ddd526b68df4efc41dbae08bccf6c7e
Naivedya Khare <rohitkhare998@gmail.com>
2024-07-01 12:53:29 +0530

    Uploaded main file containing lap price predictor code

---
+++ project.py
@@ -0,0 +1,167 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = pd.read_csv("dataset.csv")
+
+
+data.drop(columns=['Unnamed: 0'],inplace=True)
+data['Ram'] = data['Ram'].str.replace("GB", "")
+data['Weight'] = data['Weight'].str.replace("kg", "")
+data['Ram'] = data['Ram'].astype('int32')
+data['Weight'] = data['Weight'].astype('float32')
+
+
+data['Touchscreen'] = data['ScreenResolution'].apply(lambda x:1 if 'Touchscreen' in x else 0)
+
+data['Ips'] = data['ScreenResolution'].apply(lambda x:1 if 'IPS' in x else 0)
+
+def findXresolution(s):
+    return s.split()[-1].split("x")[0]
+def findYresolution(s):
+    return s.split()[-1].split("x")[1]
+
+data['X_res'] = data['ScreenResolution'].apply(lambda x: findXresolution(x))
+data['Y_res'] = data['ScreenResolution'].apply(lambda y: findYresolution(y))
+data['X_res'] = data['X_res'].astype('int')
+data['Y_res'] = data['Y_res'].astype('int')
+
+data['ppi'] = (((data['X_res']**2) + (data['Y_res']**2))**0.5/data['Inches']).astype('float')
+data.corr(numeric_only = True)['Price'].sort_values(ascending=False)
+
+data.drop(columns = ['ScreenResolution', 'Inches','X_res','Y_res'], inplace=True)
+
+def fetch_processor(x):
+    cpu_name = " ".join(x.split()[0:3])
+    if cpu_name == 'Intel Core i7' or cpu_name == 'Intel Core i5' or cpu_name == 'Intel Core i3':
+        return cpu_name
+    elif cpu_name.split()[0] == 'Intel':
+        return 'Other Intel Processor'
+    else:
+        return 'AMD Processor'
+data['Cpu_brand'] = data['Cpu'].apply(lambda x: fetch_processor(x))
+
+
+data['Memory'] = data['Memory'].astype(str).replace('.0', '', regex=True)
+data["Memory"] = data["Memory"].str.replace('GB', '')
+data["Memory"] = data["Memory"].str.replace('TB', '000')
+new = data["Memory"].str.split("+", n = 1, expand = True)
+
+data["first"] = new[0]
+data["first"] = data["first"].str.strip()
+data["second"] = new[1]
+
+data["Layer1HDD"] = data["first"].apply(lambda x: 1 if "HDD" in x else 0)
+data["Layer1SSD"] = data["first"].apply(lambda x: 1 if "SSD" in x else 0)
+data["Layer1Hybrid"] = data["first"].apply(lambda x: 1 if "Hybrid" in x else 0)
+data["Layer1Flash_Storage"] = data["first"].apply(lambda x: 1 if "Flash Storage" in x else 0)
+data['first'] = data['first'].str.replace(r'D', '')
+data["second"].fillna("0", inplace = True)
+data["Layer2HDD"] = data["second"].apply(lambda x: 1 if "HDD" in x else 0)
+data["Layer2SSD"] = data["second"].apply(lambda x: 1 if "SSD" in x else 0)
+data["Layer2Hybrid"] = data["second"].apply(lambda x: 1 if "Hybrid" in x else 0)
+data["Layer2Flash_Storage"] = data["second"].apply(lambda x: 1 if "Flash Storage" in x else 0)
+data['second'] = data['second'].str.replace(r'D', '')
+
+data["Layer2HDD"] = data["second"].apply(lambda x: 1 if "HDD" in x else 0)
+data["Layer2SSD"] = data["second"].apply(lambda x: 1 if "SSD" in x else 0)
+data["Layer2Hybrid"] = data["second"].apply(lambda x: 1 if "Hybrid" in x else 0)
+data["Layer2Flash_Storage"] = data["second"].apply(lambda x: 1 if "Flash Storage" in x else 0)
+
+data['second'] = data['second'].str.replace(r'D', '')
+
+data["first"] = int(data["first"][0].split()[0])
+data["second"] = int(data["second"][0].split()[0])
+
+data["HDD"]=(data["first"]*data["Layer1HDD"]+data["second"]*data["Layer2HDD"])
+data["SSD"]=(data["first"]*data["Layer1SSD"]+data["second"]*data["Layer2SSD"])
+data["Hybrid"]=(data["first"]*data["Layer1Hybrid"]+data["second"]*data["Layer2Hybrid"])
+data["Flash_Storage"]=(data["first"]*data["Layer1Flash_Storage"]+data["second"]*data["Layer2Flash_Storage"])
+
+data.drop(columns=['first', 'second', 'Layer1HDD', 'Layer1SSD', 'Layer1Hybrid',
+       'Layer1Flash_Storage', 'Layer2HDD', 'Layer2SSD', 'Layer2Hybrid',
+       'Layer2Flash_Storage'],inplace=True)
+
+data.drop(columns=['Hybrid','Flash_Storage','Memory','Cpu'],inplace=True)
+
+data['Gpu_brand'] = data['Gpu'].apply(lambda x:x.split()[0])
+
+data = data[data['Gpu_brand'] != 'ARM']
+data.drop(columns=['Gpu'],inplace=True)
+
+def cat_os(inp):
+    if inp == 'Windows 10' or inp == 'Windows 7' or inp == 'Windows 10 S':
+        return 'Windows'
+    elif inp == 'macOS' or inp == 'Mac OS X':
+        return 'Mac'
+    else:
+        return 'Others/No OS/Linux'
+data['os'] = data['OpSys'].apply(cat_os)
+data.drop(columns=['OpSys'],inplace=True)
+
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import r2_score,mean_absolute_error,accuracy_score
+from sklearn.ensemble import RandomForestRegressor
+import pickle
+
+X = data.drop(columns=['Price'])
+y = np.log(data['Price'])
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.15,random_state=2)
+
+step1 = ColumnTransformer(transformers=[ ('col_tnf',OneHotEncoder(drop='first'),[0,1,7,10,11])], 
+                          remainder='passthrough')
+
+step2 = RandomForestRegressor(n_estimators=100, random_state=3, max_samples=0.5, max_features=0.75, max_depth=15)
+
+pipe = Pipeline([('step1',step1), ('step2',step2)])
+pipe.fit(X_train,y_train)
+y_pred = pipe.predict(X_test)
+
+print('R2 score:',r2_score(y_test,y_pred))
+print('MAE:',mean_absolute_error(y_test,y_pred))
+
+
+data.to_csv("df.csv", index=False)
+pickle.dump(pipe,open('pipe.pkl','wb'))
+
+import streamlit as st
+
+df = pd.read_csv("df.csv")
+pipe = pickle.load(open("pipe.pkl", "rb"))
+st.title("Laptop Price Predictor")
+company = st.selectbox('Brand', df['Company'].unique())
+lap_type = st.selectbox("Type", df['TypeName'].unique())
+ram = st.selectbox("Ram(in GB)", [2,4,6,8,12,16,24,32,64])
+weight = st.number_input("Weight of the Laptop")
+touchscreen = st.selectbox("TouchScreen", ['No', 'Yes'])
+ips = st.selectbox("IPS", ['No', 'Yes'])
+screen_size = st.number_input('Screen Size')
+resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
+cpu = st.selectbox('CPU',df['Cpu_brand'].unique())
+hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+gpu = st.selectbox('GPU',df['Gpu_brand'].unique())
+os = st.selectbox('OS',df['os'].unique())
+
+if st.button('Predict Price'):
+    ppi = None
+    if touchscreen == "Yes":
+        touchscreen = 1
+    else:
+        touchscreen = 0
+    if ips == "Yes":
+        ips = 1
+    else:
+        ips = 0
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res ** 2) + (Y_res**2)) ** 0.5 / screen_size
+    query = np.array([company,lap_type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+    query = query.reshape(1, 12)
+    prediction = str(int(np.exp(pipe.predict(query)[0])))
+    st.title("The predicted price of this configuration is " + prediction)
