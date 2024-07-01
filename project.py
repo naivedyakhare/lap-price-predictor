ad6037343e6eab7b9e2c25657407b350047cc288
Naivedya Khare <rohitkhare998@gmail.com>
2024-07-01 17:59:59 +0530

    Updated the initial ML project

--- project.py
+++ project.py
@@ -28,7 +28,7 @@ data['X_res'] = data['X_res'].astype('int')
 data['Y_res'] = data['Y_res'].astype('int')
 
 data['ppi'] = (((data['X_res']**2) + (data['Y_res']**2))**0.5/data['Inches']).astype('float')
-data.corr(numeric_only = True)['Price'].sort_values(ascending=False)
+data.corr()['Price'].sort_values(ascending=False)
 
 data.drop(columns = ['ScreenResolution', 'Inches','X_res','Y_res'], inplace=True)
 
@@ -134,34 +134,26 @@ import streamlit as st
 df = pd.read_csv("df.csv")
 pipe = pickle.load(open("pipe.pkl", "rb"))
 st.title("Laptop Price Predictor")
-company = st.selectbox('Brand', df['Company'].unique())
-lap_type = st.selectbox("Type", df['TypeName'].unique())
-ram = st.selectbox("Ram(in GB)", [2,4,6,8,12,16,24,32,64])
-weight = st.number_input("Weight of the Laptop")
-touchscreen = st.selectbox("TouchScreen", ['No', 'Yes'])
-ips = st.selectbox("IPS", ['No', 'Yes'])
-screen_size = st.number_input('Screen Size')
-resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
-cpu = st.selectbox('CPU',df['Cpu_brand'].unique())
-hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
-ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
-gpu = st.selectbox('GPU',df['Gpu_brand'].unique())
-os = st.selectbox('OS',df['os'].unique())
+
+company = "Apple"
+lap_type = "Ultrabook"
+ram = 8
+weight = 2
+touchscreen = 0
+ips = 0
+resolution = "1920x1080"
+screen_size = 13
+X_res, Y_res = [int(x) for x in resolution.split('x')]
+ppi = ((X_res ** 2) + (Y_res**2)) ** 0.5 / screen_size
+cpu = "Intel Core i5"
+hdd = 1024
+ssd = 2048
+gpu = "Intel"
+os = "Mac"
 
 if st.button('Predict Price'):
-    ppi = None
-    if touchscreen == "Yes":
-        touchscreen = 1
-    else:
-        touchscreen = 0
-    if ips == "Yes":
-        ips = 1
-    else:
-        ips = 0
-    X_res = int(resolution.split('x')[0])
-    Y_res = int(resolution.split('x')[1])
-    ppi = ((X_res ** 2) + (Y_res**2)) ** 0.5 / screen_size
     query = np.array([company,lap_type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+    print(query)
     query = query.reshape(1, 12)
     prediction = str(int(np.exp(pipe.predict(query)[0])))
     st.title("The predicted price of this configuration is " + prediction)
